6011c7a81895c3865eebf8899ef2d7598d53d98c
mikearagao <miguelaragao91@gmail.com>
2015-02-18 17:55:18 +0000

    overall repository cleaning

--- vizzy_gazebo/push.sh
+++
@@ -1,5 +0,0 @@
-git add .
-git add -u
-git commit -m "$1"
-git pull
-git push -u origin master
--- vizzy_sensors/src/vizzy_sensors_filters.cpp
+++ vizzy_sensors/src/vizzy_sensors_filters.cpp
@@ -1,7 +1,7 @@
 /*
  * LICENSE: https://github.com/utexas-bwi/segbot/blob/devel/LICENSE
  */
-
+ 
 #include "vizzy_sensors/footprint_filter.h"
 #include "vizzy_sensors/nan_to_inf_filter.h"
 #include "sensor_msgs/LaserScan.h"
