2aa75c95ca953e272dd9a99e6bdc893d5aff7cf6
ruipimentelfigueiredo <ruihortafigueiredo@gmail.com>
2016-04-28 15:44:51 +0100

    yarp plugins are finally working

--- qtcreator-build/CMakeFiles/two_camera_extrinsic_calibration.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for two_camera_extrinsic_calibration.
-# This may be replaced when dependencies are built.
--- qtcreator-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vizzy/catkin_workspace/src/vizzy/vizzy_calibration
-# Build directory: /home/vizzy/catkin_workspace/src/vizzy/qtcreator-build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
