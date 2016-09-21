4e2b7de2eb9e5454aa6bc7bc6c74a329cfa8d4bd
Plinio Moreno <plinio@isr.tecnico.ulisboa.pt>
2016-09-21 18:52:06 +0100

    - Set PID parameters of left arm position controllers - Reduced joint friction in the left arm - FollowJointTrajectory actionlib server for yarp-based control working in simulation - ROS-YARP bridge of left arm's FollowJointTrajectory action working in simulation

--- vizzy_control/src/VizzyLeftArmTrajectoryActionServer.cpp
+++ vizzy_control/src/VizzyLeftArmTrajectoryActionServer.cpp
@@ -13,14 +13,15 @@
 using namespace std;
 
 VizzyLeftArmTrajectoryActionServer::VizzyLeftArmTrajectoryActionServer(const ros::NodeHandle &nh)
-    : action_server_(nh, "joint_velocity_controller",
-                     boost::bind(&VizzyLeftArmTrajectoryActionServer::actionCallback, this, _1), false)
+    : action_server_(nh, "left_arm_trajectory_controller/follow_joint_trajectory", false)
 {
-    ROS_INFO("Step 0.2");
-    info_from_bridge = node_handle_.subscribe("left_arm_trajectory_feedback", 1000, &VizzyLeftArmTrajectoryActionServer::actionBridgeCallback,this);
-    stop_execution = node_handle_.advertise<std_msgs::Bool>("left_arm_trajectory_cancel", 1000);
-    trajectory_from_move_it = node_handle_.advertise<trajectory_msgs::JointTrajectory>("left_arm_trajectory_from_moveit", 1000);
+    stop_execution = node_handle_.advertise<std_msgs::Bool>("left_arm_trajectory_cancel", 1);
+    trajectory_from_move_it = node_handle_.advertise<trajectory_msgs::JointTrajectory>("left_arm_trajectory_from_moveit", 1);
+    action_server_.registerGoalCallback(boost::bind(&VizzyLeftArmTrajectoryActionServer::goalCallback, this));
+    action_server_.registerPreemptCallback(boost::bind(&VizzyLeftArmTrajectoryActionServer::preemptCB, this));
+    info_from_bridge = node_handle_.subscribe("/left_arm_trajectory_feedback", 1, &VizzyLeftArmTrajectoryActionServer::actionBridgeCallback,this);
     action_server_.start();
+    action_active=false;
 }
 
 
@@ -30,37 +31,36 @@ VizzyLeftArmTrajectoryActionServer::~VizzyLeftArmTrajectoryActionServer()
 
 void VizzyLeftArmTrajectoryActionServer::actionBridgeCallback(const std_msgs::Int16::ConstPtr& msg)
 {
+    if (!action_active)
+        return;
     if (msg->data == 0){
       control_msgs::FollowJointTrajectoryResult result;
-      ROS_INFO("Could not complete joint angle action in the expected time.");
+      ROS_INFO("Could not reach the goal.");
       result.error_code = control_msgs::FollowJointTrajectoryResult::PATH_TOLERANCE_VIOLATED;
       action_server_.setSucceeded(result);
     }
     else if (msg->data==1){
       control_msgs::FollowJointTrajectoryResult result;
+      ROS_INFO("Goal reached!!");
       result.error_code = control_msgs::FollowJointTrajectoryResult::SUCCESSFUL;
       action_server_.setSucceeded(result);
     }
+    return;
 }
 
-void VizzyLeftArmTrajectoryActionServer::actionCallback(const control_msgs::FollowJointTrajectoryGoalConstPtr &goal)
+void VizzyLeftArmTrajectoryActionServer::goalCallback()
 {
-    ROS_INFO("Entering in callback");
-    trajectory_from_move_it.publish(goal->trajectory);
-    ROS_INFO("Trajectory sent");
-    if (action_server_.isPreemptRequested())
-    {
-      std_msgs::Bool my_bool;
-      my_bool.data = true;
-      stop_execution.publish(my_bool);
-      //preempt action server
-      action_server_.setPreempted();
-      ROS_INFO("Joint trajectory server preempted by client");
-    }
-    control_msgs::FollowJointTrajectoryResult result;
-    result.error_code = control_msgs::FollowJointTrajectoryResult::SUCCESSFUL;
-    action_server_.setSucceeded(result);
-
+    action_active=true;
+    goal_msg = action_server_.acceptNewGoal();
+    trajectory_from_move_it.publish(goal_msg->trajectory);
+    return;
 }
 
-
+void VizzyLeftArmTrajectoryActionServer::preemptCB()
+{
+   ROS_INFO("joint_velocity_controller: Preempted");
+   // set the action state to preempted
+   action_active=false;
+   action_server_.setPreempted();
+   return;
+}
