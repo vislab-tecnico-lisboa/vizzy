b7498eb3ff99977d954323caa7325fe0a14fbbe7
RafaelNunezCruz <nurafael2@gmail.com>
2016-10-06 12:58:13 +0100

    [vizzy_moveit_config] Wrist joint now in the planning group [id_selector] Added directions and points in the map

--- vizzy_ros_android_bridges/grasp_learning/src/grasp_learning.cpp
+++ vizzy_ros_android_bridges/grasp_learning/src/grasp_learning.cpp
@@ -24,132 +24,79 @@
 static const std::string OPENCV_WINDOW_L = "Left Image";
 static const std::string OPENCV_WINDOW_R = "Right Image";
 static const std::string OPENCV_WINDOW_COLOR_CONTROLS = "Color Controls";
-bool centered_complete=false, servoing=false,visible_L=false,visible_R=false;
+bool visible_L=false,visible_R=false, servoing=false;
 cv_bridge::CvImagePtr cv_ptr;
 geometry_msgs::Pose pose;
 int x_L=0,y_L=0,x_R=0,y_R=0,width=320,height=240;
 double PI=3.1416;
 double sphere_azimut=PI/2,sphere_polar=PI/2,sphere_radius=0.8;
-double angleRange=15,positionRange=5; //angle(degrees), position(cm)
 int TOL=2;
 double INCREMENT=0.01,fixationX=0,fixationY=0,fixationZ=0;
 int iLowH = 116,iHighH = 179,iLowS = 129,iHighS = 214,iLowV = 148,iHighV = 255, iErode=1,iDilate=6;
 
-double randomDouble(double min, double max){
-    int minI=(int)(min*100);
-    int maxI=(int)(max*100);
-    return (double)((rand()%(maxI-minI))+minI)/100.0;
+void callbackVoice(const std_msgs::Int8::ConstPtr& msg);
+void callbackSavIm(const std_msgs::Bool::ConstPtr& msg);
+void callbackGrasp(const std_msgs::Float32MultiArray::ConstPtr& msg);
+void callbackImage_L(const sensor_msgs::ImageConstPtr& msg);
+void callbackImage_R(const sensor_msgs::ImageConstPtr& msg);
+bool msg2Mat(cv::Mat &img, sensor_msgs::ImageConstPtr msg);
+cv::Mat imageProcessing(cv::Mat imgIn,bool &visible, int &p_x, int &p_y);
+void adjustHeadPosition(void);
+bool adjustHeadPositionPolar(void);
+bool adjustHeadPositionAzimut(void);
+bool adjustHeadPositionRadius(void);
+void moveHead(double x, double y, double z);
+bool moveArm();
+double absolute(double in);
+void transformPoint(const tf::TransformListener& listener);
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "grasp_learning");
+    ros::NodeHandle n;
+    ros::Subscriber subVoice = n.subscribe("voiceCMD", 1000, callbackVoice);
+    ros::Subscriber subGrasp = n.subscribe("graspCMD", 1000, callbackGrasp);
+    ros::Subscriber subSavIm = n.subscribe("savImCMD", 1000, callbackSavIm);
+    image_transport::ImageTransport it(n);
+    image_transport::Subscriber image_sub_L = it.subscribe("/vizzy/l_camera/image_color", 1, callbackImage_L);
+    image_transport::Subscriber image_sub_R = it.subscribe("/vizzy/r_camera/image_color", 1, callbackImage_R);
+    cv::namedWindow(OPENCV_WINDOW_L);
+    cv::namedWindow(OPENCV_WINDOW_R);
+    cv::namedWindow(OPENCV_WINDOW_COLOR_CONTROLS);
+    cv::createTrackbar("LowH"  , OPENCV_WINDOW_COLOR_CONTROLS, &iLowH  , 179);
+    cv::createTrackbar("HighH" , OPENCV_WINDOW_COLOR_CONTROLS, &iHighH , 179);
+    cv::createTrackbar("LowS"  , OPENCV_WINDOW_COLOR_CONTROLS, &iLowS  , 255);
+    cv::createTrackbar("HighS" , OPENCV_WINDOW_COLOR_CONTROLS, &iHighS , 255);
+    cv::createTrackbar("LowV"  , OPENCV_WINDOW_COLOR_CONTROLS, &iLowV  , 255);
+    cv::createTrackbar("HighV" , OPENCV_WINDOW_COLOR_CONTROLS, &iHighV , 255);
+    cv::createTrackbar("Erode" , OPENCV_WINDOW_COLOR_CONTROLS, &iErode , 6  );
+    cv::createTrackbar("Dilate", OPENCV_WINDOW_COLOR_CONTROLS, &iDilate, 6  );
+    ROS_INFO("Grasp Learning");    
+    ros::Rate loop_rate(10);
+
+    tf::TransformListener listener(ros::Duration(1));
+    ros::Timer timer = n.createTimer(ros::Duration(0.1), boost::bind(&transformPoint, boost::ref(listener)));
+
+     while (ros::ok()){ 
+        if(servoing)
+            adjustHeadPosition();
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+    cv::destroyWindow(OPENCV_WINDOW_L);
+    cv::destroyWindow(OPENCV_WINDOW_R);
+    cv::destroyWindow(OPENCV_WINDOW_COLOR_CONTROLS);
+    return 0;
 }
 
-void productQuaternion(float q[], float q1[], float q2[]){
-   float a1=q1[0],b1=q1[1],c1=q1[2],d1=q1[3];
-   float a2=q2[0],b2=q2[1],c2=q2[2],d2=q2[3];
-   q[0]=a1*a2-b1*b2-c1*c2-d1*d2;
-   q[1]=a1*b2+b1*a2+c1*d2-d1*c2;
-   q[2]=a1*c2-b1*d2+c1*a2+d1*b2;
-   q[3]=a1*d2+b1*c2-c1*b2+d1*a2;
+void callbackVoice(const std_msgs::Int8::ConstPtr& msg){
+    ROS_INFO("Voice command received");
+    int voiceCMD=(int) msg->data;
 }
 
-void randomQuaternion(float q[], int axis, double angleRange){
-   double angle = randomDouble(-angleRange,angleRange)*PI/180.0;
-   q[0]    = (float)cos(angle);
-   q[1]    = 0;
-   q[2]    = 0;
-   q[3]    = 0;
-   q[axis] = (float)sin(angle);
-   //ROS_INFO("Angle: %f",angle); 
-   //ROS_INFO("Axis: %d, Rotation:[w=%f,x=%f,y=%f,z=%f]",axis,q[0],q[1],q[2],q[3]);  
-}
-
-void modifyOrientation(float q[]){
-    float qR[4]={q[0],q[1],q[2],q[3]};
-
-    float qRx[4],qRy[4],qRz[4];
-    randomQuaternion(qRx,1,angleRange); 
-    randomQuaternion(qRy,2,angleRange);  
-    randomQuaternion(qRz,3,angleRange);  
-    productQuaternion(qR,qRz,qR);
-    productQuaternion(qR,qRx,qR);
-    productQuaternion(qR,qRy,qR);
-
-    pose.orientation.w = qR[0];  
-    pose.orientation.x = qR[1];
-    pose.orientation.y = qR[2];
-    pose.orientation.z = qR[3]; 
-
-    //ROS_INFO("nQuat: [w=%f,x=%f,y=%f,z=%f]",qR[0],qR[1],qR[2],qR[3]);  
-    //ROS_INFO("Norm: %f",sqrt(qR[0]*qR[0]+qR[1]*qR[1]+qR[2]*qR[2]+qR[3]*qR[3]));   
-}
-
-void modifyPosition(float p[]){
-    float pM[3];
-    pM[0]=p[0]+(float)(randomDouble(-positionRange,positionRange)/100.0);
-    pM[1]=p[1]+(float)(randomDouble(-positionRange,positionRange)/100.0);
-    pM[2]=p[2]+(float)(randomDouble(-positionRange,positionRange)/100.0);    
-
-    pose.position.x=pM[0];
-    pose.position.y=pM[1];
-    pose.position.z=pM[2];
-    
-    //ROS_INFO("nPos: [x=%f,y=%f,z=%f]",pM[0],pM[1],pM[2]);  
-}
-
-bool moveArm(){
-    ROS_INFO("Move arm");
-    bool success=true;
-
-    moveit::planning_interface::MoveGroup group("right_arm");
-    group.setPlannerId("RRTConnectkConfigDefault");
-    group.setGoalTolerance(0.05);
-    group.setPlanningTime(2.5);
-    group.setStartState(*group.getCurrentState());
-
-    //ROS_INFO("Reference frame: -%s-", group.getPlanningFrame().c_str());
-    //ROS_INFO("End Effector Link: -%s-", group.getEndEffectorLink().c_str());
-    //ROS_INFO("Position(HEAD) x:%f, y:%f, z:%f",fixationX,fixationY,fixationZ); 
-    //ROS_INFO("Pos [x:%f, y:%f, z:%f]",pose.position.x,pose.position.y,pose.position.z); 
-
-    float q[4];
-    q[0]=pose.orientation.w;
-    q[1]=pose.orientation.x;
-    q[2]=pose.orientation.y;
-    q[3]=pose.orientation.z;
-    //ROS_INFO("Quat: [w=%f,x=%f,y=%f,z=%f]",q[0],q[1],q[2],q[3]); 
-
-    float p[3]={pose.position.x,pose.position.y,pose.position.z};
-
-    int maxIterations=20,iteration=0;
-    do{
-       success = group.setPoseTarget(pose,"r_palm_link");
-       ROS_INFO("Set Position Target: %s", success? "SUCCESS" : "FAILED");
-       if(success){
-           ros::AsyncSpinner spinner(1);
-           spinner.start();
-           moveit::planning_interface::MoveGroup::Plan my_plan;
-           success = group.plan(my_plan);
-           ROS_INFO("Set Plan: %s", success? "SUCCESS" : "FAILED");
-           if(success){
-               int32_t error = moveit::planning_interface::MoveItErrorCode::FAILURE;
-               error=group.move();
-               if(error!= moveit::planning_interface::MoveItErrorCode::SUCCESS){
-                   success=false;
-                   ROS_INFO("Motion FAILED");
-               }
-           }
-       }
-       if(!success && iteration<maxIterations){
-           iteration++;
-           printf("\n");
-           ROS_INFO("Iteration:%d",iteration); 
-           modifyOrientation(q);
-           modifyPosition(p);
-       }
-       else  
-           ROS_INFO("Move Arm: SUCCESS");
-    }while(!success && iteration<maxIterations);
-
-    ROS_INFO("Move arm done");
-    return success;
+void callbackSavIm(const std_msgs::Bool::ConstPtr& msg){
+    ROS_INFO("Save Image command received:");
+    bool savImReceived = (bool) msg->data;  
+    servoing=true; 
 }
 
 void callbackGrasp(const std_msgs::Float32MultiArray::ConstPtr& msg){
@@ -167,70 +114,7 @@ void callbackGrasp(const std_msgs::Float32MultiArray::ConstPtr& msg){
     pose.orientation.z = (float) msg->data[8];
     pose.orientation.w = (float) msg->data[9];
 
-    servoing=true;
-}
-
-void moveHead(double x, double y, double z){
-    //ROS_INFO("Move head to x:%f, y:%f, z:%f",x,y,z);
-    actionlib::SimpleActionClient<vizzy_msgs::GazeAction> ac("gaze", true); 
-    ac.waitForServer();
-
-    vizzy_msgs::GazeGoal goal;
-    goal.type=vizzy_msgs::GazeGoal::CARTESIAN;
-    goal.fixation_point.point.x = x;
-    goal.fixation_point.point.y = y;
-    goal.fixation_point.point.z = z;
-    goal.fixation_point_error_tolerance = 0.01;
-    goal.fixation_point.header.frame_id="ego_frame";
-    goal.fixation_point.header.stamp=ros::Time::now();
-
-    ac.sendGoal(goal);
-    //ROS_INFO("Move head done");
-}
-
-void callbackSavIm(const std_msgs::Bool::ConstPtr& msg){
-    ROS_INFO("Save Image command received:");
-    bool savImReceived = (bool) msg->data;   
-    servoing=false;
-}
-
-void callbackVoice(const std_msgs::Int8::ConstPtr& msg){
-    ROS_INFO("Voice command received");
-    int voiceCMD=(int) msg->data;
-}
-
-cv::Mat imageProcessing(cv::Mat imgIn,bool &visible, int &p_x, int &p_y){
-    cv::Mat img;
-    cv::cvtColor(imgIn, img, cv::COLOR_BGR2HSV);
-    cv::inRange(img, cv::Scalar(iLowH, iLowS, iLowV), cv::Scalar(iHighH, iHighS, iHighV), img); 
-    if(iErode>0)  
-       cv::erode (img, img, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(iErode, iErode)));
-    if(iDilate>0)
-       cv::dilate(img, img, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(iDilate, iDilate))); 
-
-    cv::Moments oMoments = cv::moments(img);
-
-    visible=oMoments.m00 > 10000;
-
-    if (visible){  
-       p_x = oMoments.m10 / oMoments.m00;
-       p_y = oMoments.m01 / oMoments.m00; 
-    }
-   
-    return img;
-}
-
-bool msg2Mat(cv::Mat &img, sensor_msgs::ImageConstPtr msg){
-    try{
-      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    }
-    catch (cv_bridge::Exception& e){
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-      return false;
-    }
-
-    img=cv_ptr->image;
-    return true;
+    moveArm();
 }
 
 void callbackImage_L(const sensor_msgs::ImageConstPtr& msg){
@@ -260,11 +144,51 @@ void callbackImage_R(const sensor_msgs::ImageConstPtr& msg){
     }
 }
 
-double absolute(double in){
-    double out=in;
-    if(in<0)
-       out=-in;
-    return out;
+bool msg2Mat(cv::Mat &img, sensor_msgs::ImageConstPtr msg){
+    try{
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e){
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return false;
+    }
+
+    img=cv_ptr->image;
+    return true;
+}
+
+cv::Mat imageProcessing(cv::Mat imgIn,bool &visible, int &p_x, int &p_y){
+    cv::Mat img;
+    cv::cvtColor(imgIn, img, cv::COLOR_BGR2HSV);
+    cv::inRange(img, cv::Scalar(iLowH, iLowS, iLowV), cv::Scalar(iHighH, iHighS, iHighV), img); 
+    if(iErode>0)  
+       cv::erode (img, img, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(iErode, iErode)));
+    if(iDilate>0)
+       cv::dilate(img, img, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(iDilate, iDilate))); 
+
+    cv::Moments oMoments = cv::moments(img);
+
+    visible=oMoments.m00 > 10000;
+
+    if (visible){  
+       p_x = oMoments.m10 / oMoments.m00;
+       p_y = oMoments.m01 / oMoments.m00; 
+    }
+   
+    return img;
+}
+
+void adjustHeadPosition(void){
+    bool centered_polar=adjustHeadPositionPolar();
+    bool centered_azimut=adjustHeadPositionAzimut();
+    bool centered_radius=false;
+    if(centered_azimut)
+        centered_radius=adjustHeadPositionRadius();
+    fixationX= sphere_radius*sin(sphere_polar)*cos(sphere_azimut);
+    fixationY=-sphere_radius*cos(sphere_polar);
+    fixationZ= sphere_radius*sin(sphere_polar)*sin(sphere_azimut);
+    moveHead(fixationX,fixationY,fixationZ);
+    servoing=!(centered_polar && centered_azimut && centered_radius);
 }
 
 bool adjustHeadPositionPolar(void){
@@ -321,6 +245,86 @@ bool adjustHeadPositionRadius(void){
     return centered;
 }
 
+void moveHead(double x, double y, double z){
+    //ROS_INFO("Move head to x:%f, y:%f, z:%f",x,y,z);
+    actionlib::SimpleActionClient<vizzy_msgs::GazeAction> ac("gaze", true); 
+    ac.waitForServer();
+
+    vizzy_msgs::GazeGoal goal;
+    goal.type=vizzy_msgs::GazeGoal::CARTESIAN;
+    goal.fixation_point.point.x = x;
+    goal.fixation_point.point.y = y;
+    goal.fixation_point.point.z = z;
+    goal.fixation_point_error_tolerance = 0.01;
+    goal.fixation_point.header.frame_id="ego_frame";
+    goal.fixation_point.header.stamp=ros::Time::now();
+
+    ac.sendGoal(goal);
+    //ROS_INFO("Move head done");
+}
+
+bool moveArm(){
+    ROS_INFO("Move arm");
+    bool success=true;
+
+    moveit::planning_interface::MoveGroup group("right_arm");
+    group.setPlannerId("RRTConnectkConfigDefault");
+    group.setPlanningTime(5);
+    group.setStartState(*group.getCurrentState());
+
+    //ROS_INFO("Reference frame: -%s-", group.getPlanningFrame().c_str());
+    //ROS_INFO("End Effector Link: -%s-", group.getEndEffectorLink().c_str());
+    //ROS_INFO("Position(HEAD) x:%f, y:%f, z:%f",fixationX,fixationY,fixationZ); 
+    //ROS_INFO("Pos [x:%f, y:%f, z:%f]",pose.position.x,pose.position.y,pose.position.z); 
+    //ROS_INFO("Quat: [w=%f,x=%f,y=%f,z=%f]",pose.position.w,pose.position.x,pose.position.y,pose.orientation.z); 
+
+    success = group.setPoseTarget(pose,"r_palm_link");
+    ROS_INFO("Set Position Target: %s", success? "SUCCESS" : "FAILED");
+
+    int maxIterations=5,iteration=0;
+    double goalPositionTolerance=0.01;
+    double goalOrientationTolerance=PI/180;
+    do{
+       printf("\n");
+       ROS_INFO("Iteration:%d",iteration); 
+       group.setGoalPositionTolerance(goalPositionTolerance);
+       group.setGoalOrientationTolerance(goalOrientationTolerance);
+
+       ros::AsyncSpinner spinner(1);
+       spinner.start();
+       moveit::planning_interface::MoveGroup::Plan my_plan;
+       success = group.plan(my_plan);
+       ROS_INFO("Set Plan: %s", success? "SUCCESS" : "FAILED");
+
+       if(success){
+           int32_t error = moveit::planning_interface::MoveItErrorCode::FAILURE;
+           error=group.move();
+           if(error!= moveit::planning_interface::MoveItErrorCode::SUCCESS){
+               success=false;
+               ROS_INFO("Motion FAILED");
+           }
+       }
+       if(!success){
+           goalPositionTolerance+=0.01;
+           goalOrientationTolerance+=PI/180;
+           
+       }
+       else
+           ROS_INFO("Move Arm: SUCCESS");
+       iteration++;
+    }while(!success && iteration<=maxIterations);
+
+    ROS_INFO("Move arm done");
+    return success;
+}
+
+double absolute(double in){
+    double out=in;
+    if(in<0)
+       out=-in;
+    return out;
+}
+
 void transformPoint(const tf::TransformListener& listener){ 
     geometry_msgs::PointStamped head_point;
     head_point.header.frame_id = "ego_frame";
@@ -340,98 +344,4 @@ void transformPoint(const tf::TransformListener& listener){
     pose.position.y = base_point.point.y;
     pose.position.z = base_point.point.z;
 
-}
-
-void adjustHeadPosition(void){
-    bool centered_polar=adjustHeadPositionPolar();
-    bool centered_azimut=adjustHeadPositionAzimut();
-    bool centered_radius=false;
-    if(centered_azimut)
-        centered_radius=adjustHeadPositionRadius();
-    centered_complete=centered_polar && centered_azimut && centered_radius;
-    fixationX= sphere_radius*sin(sphere_polar)*cos(sphere_azimut);
-    fixationY=-sphere_radius*cos(sphere_polar);
-    fixationZ= sphere_radius*sin(sphere_polar)*sin(sphere_azimut);
-    moveHead(fixationX,fixationY,fixationZ);
-}
-
-void reachObject(){
-    bool success;
-    /*float p[3]={pose.position.x,pose.position.y,pose.position.z};
-    float q[4]={pose.orientation.x,pose.orientation.y,pose.orientation.z,pose.orientation.w};
-
-    //ROS_INFO("Quat: [w=%f,x=%f,y=%f,z=%f]",q[0],q[1],q[2],q[3]); 
-    angleRange=15;
-    positionRange=5;
-    pose.position.x =  0.2;
-    pose.position.y = -0.14;
-    pose.position.z =  0.8;
-    pose.orientation.x= 0.068340;
-    pose.orientation.y=-0.564690;
-    pose.orientation.z=-0.098815;
-    pose.orientation.w= 0.816511;
-    success=moveArm();
-
-    pose.position.x = p[0];
-    pose.position.y = p[1];
-    pose.position.z = p[2];
-    pose.orientation.x=q[0];
-    pose.orientation.y=q[1];
-    pose.orientation.z=q[2];
-    pose.orientation.w=q[3];
-    angleRange=5;
-    positionRange=2.5;
-    if(success)*/
-        success=moveArm();
- /*   if(success)
-        ROS_INFO("Object Reached");
-    else
-        ROS_INFO("Can not reach object");*/
-}
-
-int main(int argc, char **argv){
-    srand (time(NULL));
-    ros::init(argc, argv, "grasp_learning");
-    ros::NodeHandle n;
-    ros::Subscriber subVoice = n.subscribe("voiceCMD", 1000, callbackVoice);
-    ros::Subscriber subGrasp = n.subscribe("graspCMD", 1000, callbackGrasp);
-    ros::Subscriber subSavIm = n.subscribe("savImCMD", 1000, callbackSavIm);
-    image_transport::ImageTransport it(n);
-    image_transport::Subscriber image_sub_L = it.subscribe("/vizzy/l_camera/image_color", 1, callbackImage_L);
-    image_transport::Subscriber image_sub_R = it.subscribe("/vizzy/r_camera/image_color", 1, callbackImage_R);
-    cv::namedWindow(OPENCV_WINDOW_L);
-    cv::namedWindow(OPENCV_WINDOW_R);
-    cv::namedWindow(OPENCV_WINDOW_COLOR_CONTROLS);
-     //Create trackbars in "Control" window
-    cv::createTrackbar("LowH"  , OPENCV_WINDOW_COLOR_CONTROLS, &iLowH  , 179); //Hue (0 - 179)
-    cv::createTrackbar("HighH" , OPENCV_WINDOW_COLOR_CONTROLS, &iHighH , 179);
-    cv::createTrackbar("LowS"  , OPENCV_WINDOW_COLOR_CONTROLS, &iLowS  , 255); //Saturation (0 - 255)
-    cv::createTrackbar("HighS" , OPENCV_WINDOW_COLOR_CONTROLS, &iHighS , 255);
-    cv::createTrackbar("LowV"  , OPENCV_WINDOW_COLOR_CONTROLS, &iLowV  , 255); //Value (0 - 255)
-    cv::createTrackbar("HighV" , OPENCV_WINDOW_COLOR_CONTROLS, &iHighV , 255);
-    cv::createTrackbar("Erode" , OPENCV_WINDOW_COLOR_CONTROLS, &iErode , 6  );
-    cv::createTrackbar("Dilate", OPENCV_WINDOW_COLOR_CONTROLS, &iDilate, 6  );
-    ROS_INFO("Grasp Learning");    
-    ros::Rate loop_rate(10);
-
-    tf::TransformListener listener(ros::Duration(1));
-    ros::Timer timer = n.createTimer(ros::Duration(0.1), boost::bind(&transformPoint, boost::ref(listener)));
-
-     while (ros::ok()){ 
-        if(centered_complete){
-            servoing=false;
-            visible_L=false;
-            visible_R=false;
-            centered_complete=false;   
-            reachObject();
-        }
-        if(servoing)
-            adjustHeadPosition();
-        ros::spinOnce();
-        loop_rate.sleep();
-    }
-    cv::destroyWindow(OPENCV_WINDOW_L);
-    cv::destroyWindow(OPENCV_WINDOW_R);
-    cv::destroyWindow(OPENCV_WINDOW_COLOR_CONTROLS);
-    return 0;
 }
