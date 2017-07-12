ab7b12ceb68ae1014526e5c1c02bcdcd799a4665
Vislab @ vizzy desktop <vislablisboa@gmail.com>
2017-07-12 11:25:17 +0100

    gaze client changes

--- vizzy_control/src/GazeClient.cpp
+++ vizzy_control/src/GazeClient.cpp
@@ -46,7 +46,7 @@ int main (int argc, char **argv)
     goal.type=vizzy_msgs::GazeGoal::CARTESIAN;
     goal.fixation_point.point.x = 2;
     goal.fixation_point.point.y = 0;
-    goal.fixation_point.point.z = 1;
+    goal.fixation_point.point.z = 0;
     goal.fixation_point_error_tolerance = 0.01;
 
     while(nh.ok())
@@ -102,7 +102,7 @@ int main (int argc, char **argv)
 
 	  
 
-	goal.fixation_point.header.frame_id="base_footprint";
+	goal.fixation_point.header.frame_id="camera_link";
         goal.fixation_point.header.stamp=ros::Time::now();
     /*FIM AVELINO*/
 
