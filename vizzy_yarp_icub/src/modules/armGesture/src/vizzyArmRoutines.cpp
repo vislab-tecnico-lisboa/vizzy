79024fdd0bbcde38dee93c1f138dcdc963158ccf
Joao Avelino <joao.manuel.avelino@gmail.com>
2017-11-15 16:07:07 +0000

    Towards handshake on vizzy_yarp_icub for experiments on hri course

--- vizzy_yarp_icub/src/modules/armGesture/src/vizzyArmRoutines.cpp
+++ vizzy_yarp_icub/src/modules/armGesture/src/vizzyArmRoutines.cpp
@@ -37,7 +37,7 @@ bool VizzyArmRoutines::attach(RpcServer &source)
 bool VizzyArmRoutines::configure(yarp::os::ResourceFinder &rf) {
     _closing = false;
     /* module name */
-	Vector tmp;
+	Vector velocities_waving, velocities_stretching;
     moduleName = rf.check("name", Value("vizzyArmRoutines"),
                           "Module name (string)").asString();
     robotName = rf.check("robot", Value("vizzy"),"Robot name (string)").asString();
@@ -90,17 +90,33 @@ bool VizzyArmRoutines::configure(yarp::os::ResourceFinder &rf) {
     }
     int nj=0;
     pos->getAxes(&nj);
-	tmp.resize(nj);
+	velocities_waving.resize(nj);
+    velocities_stretching.resize(nj);
     // Setting Control Mode - Position
     for(int i=0;i< nj;i++)
         ictrl->setControlMode(i,VOCAB_CM_POSITION);
-	// Setting Motor Velocities
+	// Setting Motor Velocities for waving motion
 	for (int i=0;i< nj;i++)
-	    tmp[i] = 21.0;
-	pos->setRefSpeeds(tmp.data());
-	// 
-	pos->setRefSpeed(0,10.0); // shoulder
-	pos->setRefSpeed(4,30.0); // prosupination - wave joint
+	    velocities_waving[i] = 21.0;
+    velocities_waving[0] = 10.0; // shoulder
+    velocities_waving[4] = 30.0; // prosupination - wave joint
+
+    // Setting Motor velocities for handshake motion
+
+    velocities_stretching[0] = 15;
+    velocities_stretching[1] = 40;
+    velocities_stretching[2] = 15;
+    velocities_stretching[3] = 15;
+    velocities_stretching[4] = 15;
+    velocities_stretching[5] = 20;
+    velocities_stretching[6] = 20;
+    velocities_stretching[7] = 10;
+    velocities_stretching[8] = 20;
+    velocities_stretching[9] = 20;
+    velocities_stretching[10] = 35;
+
+
+
 
     cout << "Module Started! YEEE" << endl;
     // Definition of Poses
@@ -108,6 +124,8 @@ bool VizzyArmRoutines::configure(yarp::os::ResourceFinder &rf) {
     encoders.resize(nj);
     home_pose.resize(nj);
     wave_home_pose.resize(nj);
+    arm_forward_pose.resize(nj);
+    grabing_hand_pose.resize(nj);
     while(!encs->getEncoders(encoders.data()))
     {
             Time::delay(0.01);
@@ -126,7 +144,26 @@ bool VizzyArmRoutines::configure(yarp::os::ResourceFinder &rf) {
     wave_home_pose[6] = -0.542265*CTRL_RAD2DEG;
     wave_home_pose[7] = -0.260872*CTRL_RAD2DEG;
 
-    // Case 2
+    // Case 2 - Arm stretched
+    arm_forward_pose[0] =2.25*CTRL_RAD2DEG;
+    arm_forward_pose[1] = *CTRL_RAD2DEG;
+    arm_forward_pose[2] = 1.14261*CTRL_RAD2DEG;
+    arm_forward_pose[3] = 0.0*CTRL_RAD2DEG;
+    arm_forward_pose[4] = 0.87361*CTRL_RAD2DEG;
+    arm_forward_pose[5] = 0.033366*CTRL_RAD2DEG;
+    arm_forward_pose[6] = -0.542265*CTRL_RAD2DEG;
+    arm_forward_pose[7] = -0.260872*CTRL_RAD2DEG;
+    arm_forward_pose[8] = 37.5*CTRL_RAD2DEG;
+    arm_forward_pose[9] = 45*CTRL_RAD2DEG;
+    arm_forward_pose[10] = 69.75*CTRL_RAD2DEG;
+
+    //Case 3 - Grab hand
+    grabing_hand_pose = arm_forward_pose;
+    grabing_hand_pose[8] = 111*CTRL_RAD2DEG;
+    grabing_hand_pose[9] = 103.5*CTRL_RAD2DEG;
+    grabing_hand_pose[10] = 193.5*CTRL_RAD2DEG;
+
+
 
 	// TO BE IMPLEMENTED
     return true;
@@ -144,8 +181,26 @@ bool VizzyArmRoutines::updateModule() {
     if (commandreceived!=NULL) {
         cout << "I received command" << commandreceived->data << endl;
         switch(commandreceived->data) {
+
+            case 0:
+                //Home position
+                cout << "Going to home position" << endl;
+                pos->setRefSpeeds(velocities_stretching.data());
+                command = home_pose;
+
+                pos->positionMove(command.data());
+                while(!done) {
+                    pos->checkMotionDone(&done);
+                    Time::delay(0.00001);   // Alterado
+                }
+                done = false;
+                cout << "Went to home position" << endl;
+                break;
+
             case 1: 
                 cout << "Waving motion" << endl;
+
+                pos->setRefSpeeds(velocities_waving.data());
 
                 command = wave_home_pose;
                 pos->positionMove(command.data());
@@ -177,6 +232,37 @@ bool VizzyArmRoutines::updateModule() {
                 cout << "Waving motion DONE" << endl;
                 break;
             case 2:
+                //Stretch arm forward
+                cout << "Stretching arm forward" << endl;
+                
+                pos->setRefSpeeds(velocities_waving.data());
+                command = arm_forward_pose;
+                pos->positionMove(command.data());
+                while(!done) {
+                    pos->checkMotionDone(&done);
+                    Time::delay(0.00001);   // Alterado
+                }
+                done = false;
+                cout << "Arm stretched forward" << endl;
+                break;
+            
+            case 3:
+                //Perform handshake
+                cout << "Performing handshake..." << endl;
+                cout << "Grabbing user hand" << endl;
+
+
+                cout << "Performing waving motion" << endl;
+
+
+                cout << "Letting go of user hand" << endl;
+
+
+
+                cout << "Returning to home position" << endl;
+
+
+                cout << "Handshake performed" << endl;
                 break;
             default:
                 cout << "unknown command" << endl;
@@ -202,4 +288,11 @@ bool VizzyArmRoutines::close() {
 
     command_sub.close();
     return true;
+}
+
+void VizzyArmRoutines::setJointsVelocities()
+{
+
+
+
 }
