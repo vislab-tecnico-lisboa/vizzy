3005e8db6dfa359ce5a151db4ebf6f8360b3cc60
Plinio Moreno <plinio@isr.tecnico.ulisboa.pt>
2018-02-02 17:09:53 +0000

    Thread that reads the force sensor is working in the controller module

--- vizzy_yarp_icub/src/examples/cartesianControllerClient/ForceReadingThread.cpp
+++ vizzy_yarp_icub/src/examples/cartesianControllerClient/ForceReadingThread.cpp
@@ -1,7 +1,6 @@
 #include "ForceReadingThread.h"
 
-ForceReadingThread::ForceReadingThread(yarp::os::Subscriber<vizzy_tactile_TactSensorArray> *my_topic__):
-    semStart(0) {
+ForceReadingThread::ForceReadingThread(yarp::os::Subscriber<vizzy_tactile_TactSensorArray> *my_topic__):semStart(0) {
     setSubscriber(my_topic__);
 }
 ForceReadingThread::~ForceReadingThread(){}
@@ -13,10 +12,12 @@ bool ForceReadingThread::threadInit()
 }
 void ForceReadingThread::setSubscriber(yarp::os::Subscriber<vizzy_tactile_TactSensorArray> *my_topic__){
     my_topic = my_topic__;
+    std::cout << "after set subscriber" << std::endl;
 }
 
 void ForceReadingThread::run(){
     while(!isStopping()) {
+        //std::cout << "Before reading" << std::endl;
         vizzy_tactile_TactSensorArray reading1Mux;
         my_topic->read(reading1Mux);
         //vizzy_tactile_TactSensorArray d;
@@ -24,17 +25,17 @@ void ForceReadingThread::run(){
         //d = reading1Mux->sensorArray;
         std::vector<vizzy_tactile_TactSensor> array = reading1Mux.sensorArray;
         currForce = array[0].force;
-        std::cout << "Fx : " << currForce.x << " Fy : " << currForce.y << " Fz : " << currForce.z << std::endl;
+        //std::cout << "Fx : " << currForce.x << " Fy : " << currForce.y << " Fz : " << currForce.z << std::endl;
     }
 
 }
 
 void ForceReadingThread::get_force(yarp::sig::Vector& force){
-    semStart.wait();
+    guard.lock();
     force[0]=currForce.x;
     force[1]=currForce.y;
     force[2]=currForce.z;
-    semStart.post();
+    guard.unlock();
 }
 
 void ForceReadingThread::threadRelease()
