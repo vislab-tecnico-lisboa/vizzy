4282c8b6ae3db7adcadaf0d5cadee2493ae2ff8b
Joao Avelino <joao.manuel.avelino@gmail.com>
2018-03-12 20:14:01 +0000

    fixed timing issue in openpose demo

--- vizzy_behaviour/scripts/openpose_handshake.py
+++ vizzy_behaviour/scripts/openpose_handshake.py
@@ -5,9 +5,7 @@ import roslib
 import rospy
 
 import vizzy_msgs.msg
-
 import actionlib
-
 from openpose_ros_msgs.msg import PersonDetection_3d, BodyPartDetection_3d
 
 
@@ -17,13 +15,13 @@ class Handshaker():
         rospy.init_node('openpose_handshaker')
         self.rehablab_sub = rospy.Subscriber('/openpose_to_rehablab', PersonDetection_3d, self.callback)
         self.client = actionlib.SimpleActionClient('gaze', vizzy_msgs.msg.GazeAction)
-        client.wait_for_server()
+        self.client.wait_for_server()
 
         self.goal = vizzy_msgs.msg.GazeGoal()
         self.goal.type = vizzy_msgs.msg.GazeGoal.CARTESIAN
         self.goal.fixation_point_error_tolerance = 0.01
         self.goal.fixation_point.header.frame_id='l_camera_vision_link'
-        
+
         rospy.spin()
 
     def callback(self, person):
@@ -31,12 +29,13 @@ class Handshaker():
         if len(person.body_part) < 1:
             return
         
-        self.goal.fixation_point.header.stamp=rospy.get_rostime()
+        #self.goal.fixation_point.header.stamp=rospy.get_rostime()
+        self.goal.fixation_point.header = person.header
         self.goal.fixation_point.point.x = person.body_part[0].x
         self.goal.fixation_point.point.y = person.body_part[0].y
         self.goal.fixation_point.point.z = person.body_part[0].z
 
-        self.client.send_goal(goal)
+        self.client.send_goal(self.goal)
       
 
 
