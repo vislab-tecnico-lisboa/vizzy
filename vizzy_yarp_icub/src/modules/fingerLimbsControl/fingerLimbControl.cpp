0fc2c395e4665e6fbf62d0f26b23192062709ac7
Vislab Tecnico Lisboa <username@users.noreply.github.com>
2018-03-14 13:50:27 +0000

    pid tunning handshake

--- vizzy_yarp_icub/src/modules/fingerLimbsControl/fingerLimbControl.cpp
+++ vizzy_yarp_icub/src/modules/fingerLimbsControl/fingerLimbControl.cpp
@@ -65,11 +65,13 @@ protected:
     double inc_max;                           // max joint increment
     double joint_max;                        // max joint value for fingers (min is 0)
     double force_error;                      // accepeted force error [N]
-
+	bool make_control;
+	
     Vector sensor_force;
 
     // Controling with a PID for each motor
-    PID pid_finger = PID(0.1, 20, -20, 0.4, 0.01, 0.4); //if they are different create the 3
+    //PID pid_finger = PID(0.1, 20, -20, 3.4, 0.1, 0.5); //if they are different create the 3
+    PID pid_finger = PID(0.1, 20, -20, 0.95, 0.01, 0.0); //if they are different create the 3
 
     yarp::os::RpcServer rpcServerPort;
 
@@ -120,14 +122,14 @@ public:
         int nj=0;
         pos->getAxes(&nj);
         encoders.resize(nj);
-        double part_speeds[11] = {10.0,10.0,10.0,10.0,10.0,10.0,10.0,10.0,20.0,20.0,20.0};
+        double part_speeds[11] = {20.0,20.0,20.0,20.0,20.0,20.0,20.0,20.0,20.0,20.0,20.0};
         pos->setRefSpeeds(part_speeds);
 	std::cout << "initialization done" << std::endl;
 
-        //double finger_set[3] = {6, 5.4, 3.4};       // force setpoints 
-        finger_set[0] = 5.2; // new force setpoints 
-        finger_set[1] = 4.4;
-        finger_set[2] = 3.4;
+        //double finger_set[3] = {6.1, 5.2, 3.6};       // force setpoints 
+        finger_set[0] = 6.1; // new force setpoints 
+        finger_set[1] = 5.2;
+        finger_set[2] = 3.6;
         finger_force[0] = 0.0; // current force values
         finger_force[1] = 0.0;
         finger_force[2] = 0.0;
@@ -136,11 +138,14 @@ public:
         joint_inc[2] = 0.0;
 
         inc_max = 10;                           // max joint increment
-        joint_max = 150;                        // max joint value for fingers (min is 0)
+        joint_max = 180;                        // max joint value for fingers (min is 0)
         force_error = 0.5;                      // accepeted force error [N]
+	make_control = 0;
 
         sensor_force.resize(11); // 11 active sensors 
-
+	ictrl->setControlMode(8,VOCAB_CM_POSITION);
+	ictrl->setControlMode(9,VOCAB_CM_POSITION);
+	ictrl->setControlMode(10,VOCAB_CM_POSITION);
         rpcServerPort.open("/"+moduleName+"/rpc:i");
         attach(rpcServerPort);
 
@@ -175,6 +180,12 @@ public:
             else{   
                 sensor_force[sensor_i]= std::abs(sensor_comp[0])+std::abs(sensor_comp[1])+std::abs(sensor_comp[2]);
             }
+	    if (sensor_force[sensor_i]>10){    //just in case some sensor breaks during experiment
+		sensor_force[sensor_i]=10;
+	    }
+	    if (sensor_force[sensor_i]<0){    //just in case some sensor breaks during experiment
+		sensor_force[sensor_i]=0;
+	    }
 	            
         }
 
@@ -189,14 +200,55 @@ public:
 
         //std::cout << "Thumb: " << finger_force[0] << "Index: " << finger_force[1] << "Mid: " << finger_force[2] << std::endl;
 
-        //std::cout << "Thumb 1: " << sensor_force[0] << "2: " << sensor_force[1] << "3: " << sensor_force[2] << std::endl;
+        std::cout << "Thumb 1: " << sensor_force[0] << "2: " << sensor_force[1] << "3: " << sensor_force[2] << std::endl;
         std::cout << "Thumb - Force: " << finger_force[0]<< " Motor: " << encoders[8] << std::endl;
 
-            //std::cout << "Index 1: " << sensor_force[3] << "2: " << sensor_force[4] << "3: " << sensor_force[5] << " Total: " << finger_force[1]<< std::endl;
+        std::cout << "Index 1: " << sensor_force[3] << "2: " << sensor_force[4] << "3: " << sensor_force[5] << " Total: " << finger_force[1]<< std::endl;
         std::cout << "Index - Force: " << finger_force[1]<< " Motor: " << encoders[9] << std::endl;
 
         //std::cout << "Mid 1: " << sensor_force[7] << "2: " << sensor_force[8] << "3: " << sensor_force[9] << std::endl;
         std::cout << "Mid - Force: " << finger_force[2]<< " Motor: " << encoders[10] << std::endl;
+
+
+	if (make_control==1)
+	{
+		for (int finger_i = 0; finger_i < 3; finger_i++)
+            	{
+                //condition for doing control or keeping
+                if (std::abs(finger_force[finger_i] - finger_set[finger_i]) > force_error)
+                {
+                    double temp_pid = pid_finger.calculate(finger_set[finger_i], finger_force[finger_i]);
+
+                    //relation factor between the force and the motor angle
+                    joint_inc[finger_i] = temp_pid; // isto não dá para incluir no kp? deve dar pois
+
+                    //conditions for incrmentation - No increment larger than inc_max
+                    if (std::abs(joint_inc[finger_i]) > inc_max){
+                        joint_inc[finger_i] = inc_max*(joint_inc[finger_i]/std::abs(joint_inc[finger_i]));
+                    }
+
+                    //conditions for joint: 0 < angles < joint_max
+                    if (encoders[8+finger_i] + joint_inc[finger_i] > joint_max){
+                        joint_inc[finger_i] = joint_max-encoders[8+finger_i]; 
+                    }
+                    if (encoders[8+finger_i] + joint_inc[finger_i] < 0){
+                        joint_inc[finger_i] = 0 - encoders[8+finger_i];
+                    }
+                    //std::cout << "Value: " << encoders[8+finger_i] << "Increment: " << joint_inc[finger_i] << std::endl;      
+                }
+                else
+                {
+                    joint_inc[finger_i]=0;
+                    //std::cout << "Finger " << finger_i << " is ok!" << std::endl;
+                }
+            	} // end for
+
+            //change the enconders 8, 9 and 10 
+            pos->positionMove(8,encoders[8]+joint_inc[0]);
+            pos->positionMove(9,encoders[9]+joint_inc[1]);
+            pos->positionMove(10,encoders[10]+joint_inc[2]);
+
+	}
 
         return true;
     }
@@ -224,48 +276,15 @@ public:
     {
         if (type=="one")
         {
-            for (int finger_i = 0; finger_i < 3; finger_i++)
-            {
-                //condition for doing control or keeping
-                if (std::abs(finger_force[finger_i] - finger_set[finger_i]) > force_error)
-                {
-                    double temp_pid = pid_finger.calculate(finger_set[finger_i], finger_force[finger_i]);
-
-                    //relation factor between the force and the motor angle
-                    joint_inc[finger_i] = temp_pid * 10; // isto não dá para incluir no kp? deve dar pois
-
-                    //conditions for incrmentation - No increment larger than inc_max
-                    if (std::abs(joint_inc[finger_i]) > inc_max){
-                        joint_inc[finger_i] = inc_max*(joint_inc[finger_i]/std::abs(joint_inc[finger_i]));
-                    }
-
-                    //conditions for joint: 0 < angles < joint_max
-                    if (encoders[8+finger_i] + joint_inc[finger_i] > joint_max){
-                        joint_inc[finger_i] = joint_max-encoders[8+finger_i]; 
-                    }
-                    if (encoders[8+finger_i] + joint_inc[finger_i] < 0){
-                        joint_inc[finger_i] = 0 - encoders[8+finger_i];
-                    }
-                    //std::cout << "Value: " << encoders[8+finger_i] << "Increment: " << joint_inc[finger_i] << std::endl;      
-                }
-                else
-                {
-                    joint_inc[finger_i]=0;
-                    //std::cout << "Finger " << finger_i << " is ok!" << std::endl;
-                }
-            } // end for
-
-            //change the enconders 8, 9 and 10 
-            pos->positionMove(8,encoders[8]+joint_inc[0]);
-            pos->positionMove(9,encoders[9]+joint_inc[1]);
-            pos->positionMove(10,encoders[10]+joint_inc[2]);
+		make_control=1; 
         }
         else if (type=="two")
         {
-            // TODO tiago
-            const double grabTwo8 = 0.0;
-            const double grabTwo9 = 0.0;
-            const double grabTwo10 = 0.0;
+		make_control=0;
+            // handshake constant
+            const double grabTwo8 = 118;
+            const double grabTwo9 = 105;
+            const double grabTwo10 = 185;
 
             pos->positionMove(8, grabTwo8);
             pos->positionMove(9, grabTwo9);
@@ -281,14 +300,31 @@ public:
 
     bool release()
     {
-        // TODO tiago
-        const double releaseTwo8 = 0.0;
-        const double releaseTwo9 = 0.0;
-        const double releaseTwo10 = 0.0;
+        // position pre-handshake
+	make_control=0;
+        const double release0 = -3.6;
+        const double release1 = 73.5;
+	const double release2 = 0.0;
+        const double release3 = -8.5;
+        const double release4 = 47.25;
+	const double release5 = -10.2;
+        const double release6 = 28;
+        const double release7 = 18.2;
+	const double release8 = 37.5;
+        const double release9 = 45;
+        const double release10 = 69.75;
 
-        pos->positionMove(8, releaseTwo8);
-        pos->positionMove(9, releaseTwo9);
-        pos->positionMove(10, releaseTwo10);
+        pos->positionMove(0, release0);
+        pos->positionMove(1, release1);
+	pos->positionMove(2, release2);
+        pos->positionMove(3, release3);
+        pos->positionMove(4, release4);
+	pos->positionMove(5, release5);
+        pos->positionMove(6, release6);
+        pos->positionMove(7, release7);
+	pos->positionMove(8, release8);
+        pos->positionMove(9, release9);
+        pos->positionMove(10, release10);
 
         return true;
     }
