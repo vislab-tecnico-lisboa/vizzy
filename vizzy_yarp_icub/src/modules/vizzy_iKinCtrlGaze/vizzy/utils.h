25763691d06e7d6a34288962f6636eacbcb177c9
Plinio Moreno <plinio@isr.tecnico.ulisboa.pt>
2018-04-10 21:33:41 +0100

    Revert "Gaze controller upgraded"

--- vizzy_yarp_icub/src/modules/vizzy_iKinCtrlGaze/vizzy/utils.h
+++ vizzy_yarp_icub/src/modules/vizzy_iKinCtrlGaze/vizzy/utils.h
@@ -1,9 +1,8 @@
 /* 
  * Copyright (C) 2010 RobotCub Consortium, European Commission FP6 Project IST-004370
- * Copyright (C) 2011 Computer and Robot Vision Laboratory
- * Author: Ugo Pattacini, Alessandro Roncone, Plinio Moreno, Duarte Aragão
- * email:  ugo.pattacini@iit.it, alessandro.roncone@iit.it, plinio@isr.tecnico.ulisboa.pt, daragao@gmail.com
- * website: http://vislab.isr.tecnico.ulisboa.pt
+ * Author: Ugo Pattacini
+ * email:  ugo.pattacini@iit.it
+ * website: www.robotcub.org
  * Permission is granted to copy, distribute, and/or modify this program
  * under the terms of the GNU General Public License, version 2 or any
  * later version published by the Free Software Foundation.
@@ -23,14 +22,18 @@
 #include <string>
 #include <algorithm>
 
-#include <yarp/os/all.h>
-#include <yarp/dev/all.h>
-#include <yarp/sig/all.h>
+#include <yarp/os/Thread.h>
+#include <yarp/os/Event.h>
+#include <yarp/os/Semaphore.h>
+#include <yarp/sig/Vector.h>
+#include <yarp/sig/Matrix.h>
 #include <yarp/math/Math.h>
-#include <yarp/math/SVD.h>
+#include <yarp/dev/PolyDriver.h>
+#include <yarp/os/ResourceFinder.h>
 
 #include <vizzy/gazeNlp.h>
-#include <vizzy/vizzyFwd.h>
+
+#define ALMOST_ZERO     1e-6
 
 using namespace std;
 using namespace yarp::os;
@@ -41,8 +44,8 @@ using namespace iCub::ctrl;
 using namespace iCub::iKin;
 
 
-// This class handles the incoming fixation point
-// xyz coordinates.
+// This inherited class handles the incoming
+// fixation point xyz coordinates.
 // 
 // Since it accepts a bottle, it is possible to 
 // issue the command "yarp read /sender /ctrlName/xd:i"
@@ -56,52 +59,51 @@ class xdPort : public BufferedPort<Bottle>,
 protected:
     void  *slv;
 
-    Mutex  mutex_0;
-    Mutex  mutex_1;
-    Event  triggerNeck;
-    Vector xd;
-    Vector xdDelayed;    
-    bool   isNew;
-    bool   isNewDelayed;
-    bool   locked;
-    bool   closing;
-    int    rx;
+    Semaphore mutex_0;
+    Semaphore mutex_1;
+    Event     syncEvent;
+    Vector    xd;
+    Vector    xdDelayed;    
+    bool      isNew;
+    bool      isNewDelayed;
+    bool      closing;
+    int       rx;
 
     void onRead(Bottle &b);
     void run();
 
 public:
-    xdPort(void *_slv);
+    xdPort(const Vector &xd0, void *_slv);
     ~xdPort();
 
-    void    init(const Vector &xd0);
-    void    lock()           { locked=true;         }
-    void    unlock()         { locked=false;        }
-    bool    islocked() const { return locked;       }
     int     get_rx() const   { return rx;           }
     bool   &get_new()        { return isNew;        }
     bool   &get_newDelayed() { return isNewDelayed; }    
-    bool    set_xd(const Vector &_xd);
+    void    set_xd(const Vector &_xd);
     Vector  get_xd();
     Vector  get_xdDelayed();
 };
 
 
-// This class handles the data exchange among components.
-class ExchangeData
+// This class handles the data exchange
+// between Solvers and Controller objects.
+class exchangeData
 {
 protected:
-    Mutex  mutex[9];
-    
+    Semaphore mutex[8];
+
     Vector xd,qd;
     Vector x,q,torso;
     Vector v,counterv;
     Matrix S;
-    Vector imu;
+    bool   isCtrlActive;
+    bool   canCtrlBeDisabled;
+    bool   saccadeUnderway;
+    double minAllowedVergence;
     double x_stamp;
 
 public:
-    ExchangeData();
+    exchangeData();
 
     void    resize_v(const int sz, const double val);
     void    resize_counterv(const int sz, const double val);
@@ -116,7 +118,6 @@ public:
     void    set_v(const Vector &_v);
     void    set_counterv(const Vector &_counterv);
     void    set_fpFrame(const Matrix &_S);
-    void    set_imu(const Vector &_imu);
 
     Vector  get_xd();
     Vector  get_qd();
@@ -127,107 +128,56 @@ public:
     Vector  get_v();
     Vector  get_counterv();
     Matrix  get_fpFrame();
-    Vector  get_imu();
 
-    string  headVersion2String();
-
-    // data members that do not need protection
-    xdPort         *port_xd;
-    string          robotName;
-    string          localStemName;
-    Vector          eyeTiltLim;
-    double          minAllowedVergence;
-    double          eyesBoundVer;
-    double          gyro_noise_threshold;
-    double          stabilizationGain;
-    double          head_version;
-    double          saccadesInhibitionPeriod;
-    double          saccadesActivationAngle;
-    int             neckSolveCnt;
-    bool            ctrlActive;
-    bool            trackingModeOn;
-    bool            saccadeUnderway;
-    bool            verbose;
-    bool            tweakOverwrite;
-    bool            saccadesOn;
-    bool            neckPosCtrlOn;
-    bool            stabilizationOn;
-    ResourceFinder  rf_cameras;
-    ResourceFinder  rf_tweak;
-    string          tweakFile;
-    bool            debugInfoEnabled;    
+    bool   &get_isCtrlActive()       { return isCtrlActive;       }
+    bool   &get_canCtrlBeDisabled()  { return canCtrlBeDisabled;  }
+    bool   &get_isSaccadeUnderway()  { return saccadeUnderway;    }
+    double &get_minAllowedVergence() { return minAllowedVergence; }
 };
 
 
-// This class handles the incoming IMU data
-class IMUPort : public BufferedPort<Vector>
-{
-protected:
-    ExchangeData *commData;
-    void onRead(Vector &imu);
-
-public:
-    IMUPort();
-    void setExchangeData(ExchangeData *commData);
-};
-
-
-// This class defines gaze components such as
-// controller, localizer, solver ...
-class GazeComponent
-{
-protected:
-    vizzyEye *eyeL;
-    vizzyEye *eyeR;
-
-public:
-    virtual bool getExtrinsicsMatrix(const string &type, Matrix &M);
-    virtual bool setExtrinsicsMatrix(const string &type, const Matrix &M);
-    virtual void minAllowedVergenceChanged() { }
-};
-
-
-// Saturate val between min and max.
+// saturate value val between min and max
 inline double sat(const double val, const double min, const double max)
 {
     return std::min(std::max(val,min),max);
 }
 
 
-// Allocates Projection Matrix Prj for the camera read from cameras::file;
-// type is in {"CAMERA_CALIBRATION_LEFT","CAMERA_CALIBRATION_RIGHT"}.
-// Returns true if correctly configured.
-bool getCamParams(const ResourceFinder &rf, const string &type, Matrix **Prj, int &w, int &h, const bool verbose=false);
+// Allocates Projection Matrix Prj for the camera read from camerasFile
+// type is in {"CAMERA_CALIBRATION_LEFT","CAMERA_CALIBRATION_RIGHT"}
+// Returns true if correctly configured
+bool getCamPrj(const ResourceFinder &rf, const string &type, Matrix **Prj);
 
 
-// Allocates the two aligning matrices read from cameras::file;
-// type is in {"ALIGN_KIN_LEFT","ALIGN_KIN_RIGHT"}.
-// Returns true if correctly configured.
-bool getAlignHN(const ResourceFinder &rf, const string &type, iKinChain *chain, const bool verbose=false);
+// Allocates the two aligning matrices read from camerasFile
+// type is in {"ALIGN_KIN_LEFT","ALIGN_KIN_RIGHT"}
+// Returns true if correctly configured
+bool getAlignHN(const ResourceFinder &camerasFile, const string &type, iKinChain *chain, const bool verbose);
 
 
 // Aligns head joints bounds with current onboard bounds.
-// Returns a matrix containing the actual limits.
+// Returns a matrix containing the actual limits
 Matrix alignJointsBounds(iKinChain *chain, PolyDriver *drvTorso, PolyDriver *drvHead,
-                         const Vector &eyeTiltLim);
+                         const double eyeTiltMin, const double eyeTiltMax);
 
 
-// Copies joints bounds from first chain to second chain.
+// Copies joints bounds from first chain to second chain
 void copyJointsBounds(iKinChain *ch1, iKinChain *ch2);
 
 
-// Updates torso blocked joints values within the chain.
+// Updates torso blocked joints values within the chain
 void updateTorsoBlockedJoints(iKinChain *chain, const Vector &fbTorso);
 
 
-// Updates neck blocked joints values within the chain.
+// Updates neck blocked joints values within the chain
 void updateNeckBlockedJoints(iKinChain *chain, const Vector &fbNeck);
 
 
 // Reads encoders values.
 // Returns true if communication with robot is stable, false otherwise.
 bool getFeedback(Vector &fbTorso, Vector &fbHead, PolyDriver *drvTorso,
-                 PolyDriver *drvHead, ExchangeData *commData, double *timeStamp=NULL);
+                 PolyDriver *drvHead, exchangeData *commData, double *timeStamp=NULL);
+
 
 #endif
 
