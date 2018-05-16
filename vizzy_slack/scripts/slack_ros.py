ce91a6a4a48092b55c0c7d9563b25c88eb1eba99
Rui Figueiredo <ruimgf95@gmail.com>
2018-05-16 15:19:05 +0100

    audio integration

--- vizzy_slack/scripts/slack_ros.py
+++ vizzy_slack/scripts/slack_ros.py
@@ -14,15 +14,16 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-    
+
 # Import required Python code.
 import time
 import roslib
 import rospy
 from std_msgs.msg import String
 import sys
-
+import json
 import requests
+
 
 from slackclient import SlackClient
 
@@ -34,47 +35,46 @@ class SlackROS():
         self.token = rospy.get_param('~token', 'xoxp-123456789')
         self.channel = rospy.get_param('~channel', 'G1234567Q')
         self.username = rospy.get_param('~username', 'ros-bot')
-	
+
         # Create a publisher for our custom message.
-        pub = rospy.Publisher('from_slack_to_ros', String, queue_size=10)
-	rospy.Subscriber("from_ros_to_slack", String, self.callback)
-	rospy.Subscriber("send_file_to_slack", String, self.filecallback)
+        pub = rospy.Publisher('/vizzy/slack/from_slack_to_ros', String, queue_size=10)
+        rospy.Subscriber("/vizzy/slack/from_ros_to_slack", String, self.callback)
+        rospy.Subscriber("/vizzy/slack/send_file_to_slack", String, self.filecallback)
 
-	# Create the slack client
-	self.sc = SlackClient(self.token)
+    	# Create the slack client
+    	self.sc = SlackClient(self.token)
 
-	if self.sc.rtm_connect():
+    	if self.sc.rtm_connect():
 
             # Main while loop.
             while not rospy.is_shutdown():
                 for reply in self.sc.rtm_read():
                     if "type" in reply and "user" in reply:
-                        #print reply
                         if reply["type"] == "message" and reply["channel"] == self.channel:
                             pub.publish(reply["text"])
-                
-	        # Sleep for a while before publishing new messages. Division is so rate != period.
+
+            # Sleep for a while before publishing new messages. Division is so rate != period.
                 rospy.sleep(2.0)
 
     def callback(self, data):
-	self.sc.api_call(
-    	    "chat.postMessage", channel=self.channel, text=data.data,
-    	    username=self.username, icon_emoji=':robot_face:'
-	)
+    	self.sc.api_call(
+        	    "chat.postMessage", channel=self.channel, text=data.data,
+        	    username=self.username, icon_emoji=':robot_face:'
+    	)
         #rospy.loginfo(rospy.get_caller_id() + "I heard %s %s", data.data, self.channel)
 
     def filecallback(self, data):
         with open(data.data, 'rb') as file:
-                r = requests.post('https://slack.com/api/files.upload', files={'file': ['File '+data.data, file]}, params={
-                                        'token': self.token,
-                                        'channels': self.channel
-                })
+            r = requests.post('https://slack.com/api/files.upload', files={'file': ['File '+data.data, file]}, params={
+                                    'token': self.token,
+                                    'channels': self.channel
+            })
 
 
 # Main function.
 if __name__ == '__main__':
     # Initialize the node and name it.
-    rospy.init_node('slack_ros')
+    rospy.init_node('slack_ros',log_level=rospy.DEBUG)
     # Go to class functions that do all the heavy lifting. Do error checking.
     try:
         sr = SlackROS()
