71067a9622750ea32615dd6167c492d8464e077b
Pedro Vicente <pvicente@isr.tecnico.ulisboa.pt>
2018-07-17 11:08:47 +0100

    [vizzy-KinMatlab] Updating the DH parameters on the fingers

--- vizzy_description/kinematic_description_matlab/right_wrist_fingers_fwd_kin/RightWristIndexFwdKinVizzy.m
+++ vizzy_description/kinematic_description_matlab/right_wrist_fingers_fwd_kin/RightWristIndexFwdKinVizzy.m
@@ -17,25 +17,31 @@ rpy_Rn = [];
 % [roll,pitch,yaw] = dcm2angle(G_00(1:3,1:3)','ZYX');
 % rpy_Rn = [rpy_Rn; roll,pitch,yaw];
 %			a(R)		d	alpha	theta
-G_01=evalDHMatrix(	-122.87,	4.5,	pi/2,	itheta(1)+atan2(18.595,76.46+.295+44.7));%VIZZY WAIST
+%G_01=evalDHMatrix(	-122.87,	4.5,	pi/2,	itheta(1)+atan2(18.595,76.46+.295+44.7));%VIZZY WAIST
+
+G_01=evalDHMatrix(	-122.87,	4.5,	pi/2, +atan2(18.595,76.46+.295+44.7));%*evalDHMatrix(0,	0,	0,	itheta(1));%VIZZY WAIST
+% G_01_=;%VIZZY WAIST
 [roll,pitch,yaw] = dcm2angle(G_01(1:3,1:3)','ZYX');
 % rpy_Rn = [rpy_Rn; roll,pitch,yaw];
- G_sL0 = G_00;%[1 0 0 0; 0 0 1 0; 0 1 0 0; 0 0 0 1]
+ %G_sL0 = G_00;%[1 0 0 0; 0 0 1 0; 0 1 0 0; 0 0 0 1]
+G_sL0 = root_to_wrist_mat*G_00;
 G_sL1 = G_sL0*G_01;
 
-G_12=evalDHMatrix(	-33,	0,	0,	itheta(2));
+G_12=evalDHMatrix(	-33,	0,	0,	itheta(1));
 [roll,pitch,yaw] = dcm2angle(G_12(1:3,1:3)','ZYX');
 rpy_Rn = [rpy_Rn; roll,pitch,yaw];
 
-G_34=evalDHMatrix(	-26.5,	0,	0,	itheta(3));
+G_34=evalDHMatrix(	-26.5,	0,	0,	itheta(2));
 [roll,pitch,yaw] = dcm2angle(G_34(1:3,1:3)','ZYX');
 rpy_Rn = [rpy_Rn; roll,pitch,yaw];
+
+G_45 = evalDHMatrix(	-28.48,	0,	0,	itheta(3));
 % 
 G_sL2 = G_sL1*G_12;
-G_sL4   = G_sL2  * G_34;
-% 
+G_sL4 = G_sL2  * G_34;
+G_sL5 = G_sL4 * G_45; % 
 % T_Ro0 = G_sL0;
-T_0n  = G_01*G_12*G_34;
+T_0n  = G_00*G_01*G_12*G_34*G_45;
 
 if (display==1)
     jnt2 = DrawCylinder(ljnt, rjnt, G_sL0*[1 0 0 0; 0 1 0 0; 0 0 1 -ljnt/2; 0 0 0 1], JntColor, 100, 0.5);
@@ -50,6 +56,7 @@ if (display==1)
 %     hold on
     jnt3 = DrawCylinder(ljnt, rjnt, G_sL4*[1 0 0 0; 0 1 0 0; 0 0 1 -ljnt/2; 0 0 0 1], JntColor, 100, 0.5);
     DrawRefFrame(G_sL4,3)
+    DrawRefFrame(G_sL5,4)
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     axis('equal')
     xlabel('x')
