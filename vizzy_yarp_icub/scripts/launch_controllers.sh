73fce095304a40d6046d91ec467c5da804bb08e7
João Avelino <joao.avelino@tecnico.ulisboa.pt>
2018-11-06 17:24:42 +0000

    Add correct order on launch_controllers.sh and sleep. Change default move_it to true on vizzy_simulatior (YARP wont work otherwise)

--- vizzy_yarp_icub/scripts/launch_controllers.sh
+++ vizzy_yarp_icub/scripts/launch_controllers.sh
@@ -5,6 +5,8 @@ vizzyUpperBodyJointsSim &
 cartesianControllerServer --from server_right_arm_sim.ini &
 cartesianSolver --from solver_right_arm_sim.ini &
 vizzy_iKinGazeCtrl --from config_640x480_Sim.ini &
-gazeSimulationBridge &
 fixationPointStatusBridge &
-fixationPointActionBridge
+sleep 1
+fixationPointActionBridge &
+sleep 1
+gazeSimulationBridge
