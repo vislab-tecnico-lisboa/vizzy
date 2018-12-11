cd0e4634246783ca15fa1550aa99b10a6d8f47ba
João Avelino <joao.avelino@tecnico.ulisboa.pt>
2018-12-11 14:58:52 +0000

    Usable grasp action rviz plugin

--- vizzy_rviz_plugins/include/vizzy_rviz_plugins/grasp_panel.hpp
+++ vizzy_rviz_plugins/include/vizzy_rviz_plugins/grasp_panel.hpp
@@ -12,11 +12,15 @@ December, 2018
 #include <actionlib/client/simple_action_client.h>
 #include <QWidget>
 #include <QPushButton>
+#include <QGroupBox>
+#include <QLabel>
+#include <QSpinBox>
 #include <memory.h>
 #include <vizzy_msgs/CartesianAction.h>
 #include <vizzy_msgs/CartesianActionGoal.h>
 #include <vizzy_msgs/CartesianActionFeedback.h>
 #include <vizzy_msgs/CartesianActionResult.h>
+#include <geometry_msgs/PoseStamped.h>
 
 typedef actionlib::SimpleActionClient<vizzy_msgs::CartesianAction> cartesian_client;
 
@@ -38,40 +42,69 @@ class GraspPanel: public rviz::Panel
 
 protected:
 
-  // One-line text editor for entering the outgoing ROS topic name.
-  QLineEdit* output_topic_editor_;
-   // The current name of the output topic.
+  // One-line text editor for entering the outgoing ROS action name.
+   QLineEdit* output_action_editor_;
+   // The current name of the output action.
    QString output_action_;
-   //Push buttons for grasp commands
+
+  // One-line text editor for entering the outgoing ROS topic name.
+   QLineEdit* input_topic_editor_;
+   // The current name of the output topic.
+   QString input_topic_;
+   
 
    // The ROS publisher for the grasp command
     ros::NodeHandle nh_;
+    ros::Subscriber goal_sub_;
+
     std::shared_ptr<cartesian_client> ac;
 
    
 private:
-   QPushButton *home_button;
-   QPushButton *grab_button;
-   QPushButton *release_button;
-   QPushButton *putinbox_button;
-   QPushButton *giveaway_button;
-   QPushButton *go_to_goal;
+  //Push buttons for grasp commands
+  QPushButton *home_button;
+  QPushButton *grab_button;
+  QPushButton *release_button;
+  QPushButton *putinbox_button;
+  QPushButton *giveaway_button;
+  QPushButton *go_to_goal;
+  QPushButton *freeze_goal_button_;
 
+  //Controls group
+  bool freeze_goal_ = false;
+  void createControls(const QString &title);
+  QGroupBox *controlsGroup_;
+  QLabel *x_label_;
+  QLabel *y_label_;
+  QLabel *z_label_;
 
-   //Current Goal
-   double goal_pos_x_;
-   double goal_pos_y_;
-   double goal_pos_z_;
-   double goal_orient_x_;
-   double goal_orient_y_;
-   double goal_orient_z_;
-   double goal_orient_w_;
+  QSpinBox *x_spin_;
+  QSpinBox *y_spin_;
+  QSpinBox *z_spin_;
+  
+  //Current Goal
+  double goal_pos_x_;
+  double goal_pos_y_;
+  double goal_pos_z_;
+  double goal_orient_x_;
+  double goal_orient_y_;
+  double goal_orient_z_;
+
+  double goal_pos_x_offset_ = 0;
+  double goal_pos_y_offset_ = 0;
+  double goal_pos_z_offset_ = 0;
+  double goal_orient_x_offset_ = 0;
+  double goal_orient_y_offset_ = 0;
+  double goal_orient_z_offset_ = 0; 
+
+  void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg);
 
 
 
 
 public Q_SLOTS:
 
+  void setAction( const QString& action );
   void setTopic( const QString& topic );
   void home();
   void grab();
@@ -79,11 +112,15 @@ public Q_SLOTS:
   void putinbox();
   void giveaway();
   void gotoGoal();
+  void updateGoalX();
+  void updateGoalY();
+  void updateGoalZ();
 
 protected Q_SLOTS:
 
+  void updateAction();
   void updateTopic();
-
+  void freezeUnfreeze();
 
 };
 
