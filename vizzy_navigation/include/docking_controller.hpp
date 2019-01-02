74a4f89bac39ad2f93ce943e015acc9d4c8f58f0
João Avelino <joao.avelino@tecnico.ulisboa.pt>
2019-01-02 18:41:50 +0000

    Docking controller implemented and tested. TODO: Docking station perception and final docking

--- vizzy_navigation/include/docking_controller.hpp
+++ vizzy_navigation/include/docking_controller.hpp
@@ -1,20 +1,90 @@
+/*Joao Avelino
+January 2019*/
+
 #ifndef DOCKING_CONTROLLER_HPP_
 #define DOCKING_CONTROLLER_HPP_
+#include <string>
+
+
+namespace docking_ctrl{
+
+class ControlException : public std::exception
+{   
+
+public:
+
+    ControlException(const std::string& msg) : m_msg(msg) {}
+
+    virtual const char* what() const throw()
+    {
+        return m_msg.c_str();
+    }
+
+    const std::string m_msg;
+};
+
+class Pose2D
+{
+public:
+    Pose2D(){}
+    Pose2D(float x, float y, float theta) : 
+        x_(x), y_(y), theta_(theta){}
+    
+    Pose2D(float x, float y, float theta, std::string frame_id) : 
+        x_(x), y_(y), theta_(theta), frame_id_(frame_id){}
+    float x_;
+    float y_;
+    float theta_;
+    std::string frame_id_;
+};
+
+class ControlSignal
+{
+public:
+    ControlSignal() : linear_vel_(0), angular_vel_(0){};
+    ControlSignal(float v, float w) : linear_vel_(v), angular_vel_(w){};
+    float linear_vel_;
+    float angular_vel_;
+};
 
 
 class DockingController
 {
 private:
+
     float k_ro_;
     float k_alpha_;
     float k_beta_;
+    Pose2D robot_pose_;
+    Pose2D goal_;
+    float lin_vel_sat_;
+    float ang_vel_sat_;
+    
+
+
 
 public:
+    DockingController(float k_ro, float k_alpha, float k_beta, Pose2D goal, Pose2D robot_pose);
+    DockingController(float k_ro, float k_alpha, float k_beta, float lin_vel_sat, float ang_vel_sat);
     DockingController(float k_ro, float k_alpha, float k_beta);
     ~DockingController();
     void updateGains(float k_ro, float k_alpha, float k_beta);
-    float computeControlSignal(float ro, float alpha, float beta);
+    void updateSaturations(float lin_vel_sat_, float ang_vel_sat_);
+    ControlSignal computeControlSignal();
+    Pose2D getCurrentPose();
+    Pose2D getCurrentGoal();
+
+    
+    void updateRobotPose(float x, float y, float theta, std::string frame_id);
+    void updateRobotPose(Pose2D newPose);
+    void updateGoal(float x, float y, float theta, std::string frame_id);
+    void updateGoal(Pose2D newGoal);
+
+    bool running_;
+
 };
 
+//END namespace docking_ctrl
+}
 
 #endif
--- vizzy_navigation/src/docking_controller.cpp
+++ vizzy_navigation/src/docking_controller.cpp
@@ -1,24 +1,177 @@
+/*Joao Avelino
+January 2019*/
+
 #include <docking_controller.hpp>
+#include <iostream>
+#include <math.h>
+
+namespace docking_ctrl{
+
+DockingController::DockingController(float k_ro, float k_alpha, float k_beta, Pose2D goal, Pose2D robot_pose) : k_ro_(k_ro), 
+        k_alpha_(k_alpha), k_beta_(k_beta), robot_pose_(robot_pose), goal_(goal), 
+        lin_vel_sat_(2.0), ang_vel_sat_(2.0*M_PI), running_(false)
+{}
 
 DockingController::DockingController(float k_ro, float k_alpha, float k_beta) : k_ro_(k_ro), 
-        k_alpha_(k_alpha), k_beta_(k_beta)
-{
+        k_alpha_(k_alpha), k_beta_(k_beta), robot_pose_(0.0, 0.0, 0.0), goal_(0.0, 0.0, 0.0), 
+        lin_vel_sat_(2.0), ang_vel_sat_(2.0*M_PI), running_(false)
+{}
 
-}
+
+DockingController::DockingController(float k_ro, float k_alpha, float k_beta, float lin_vel_sat, 
+        float ang_vel_sat) : k_ro_(k_ro), k_alpha_(k_alpha), k_beta_(k_beta), robot_pose_(0.0, 0.0, 0.0), 
+        goal_(0.0, 0.0, 0.0), lin_vel_sat_(lin_vel_sat), ang_vel_sat_(ang_vel_sat), running_(false)
+{}
+
 
 DockingController::~DockingController(){}
 
 
+
+
 void DockingController::updateGains(float k_ro, float k_alpha, float k_beta)
 {
-
+    k_ro_ = k_ro;
+    k_beta_ = k_beta;    
+    k_alpha_ = k_alpha;
 }
 
 
-float DockingController::computeControlSignal(float ro, float alpha, float beta)
+/*Pose controller described in Introduction of Autonomous Mobile Robots by Illah Reza
+Noubakhsh and Roland Siegwart.*/
+
+ControlSignal DockingController::computeControlSignal()
 {
 
+    if(!running_)
+        return ControlSignal(0, 0);
 
+    /*Both poses must be relative to the same frame (and cannot be null frame_id)*/
+    if(robot_pose_.frame_id_ != goal_.frame_id_ || 
+        robot_pose_.frame_id_.length() == 0 || goal_.frame_id_.length() == 0)
+            throw ControlException("Robot pose and goal pose should be relative to the same non-null frame_id.");
+
+    /*Convertion to the goal frame*/
+    /*We have the transformation from goal to common frame [R | t], so its inverse is [R^T | - R^T*t] */
+    /*This code is completely tested! Do NOT change!*/
+    float theta = robot_pose_.theta_-goal_.theta_;
+    float x_rp = cosf(goal_.theta_)*robot_pose_.x_+sinf(goal_.theta_)*robot_pose_.y_-cosf(goal_.theta_)*goal_.x_-sinf(goal_.theta_)*goal_.y_;
+    float y_rp = -sinf(goal_.theta_)*robot_pose_.x_+cosf(goal_.theta_)*robot_pose_.y_+sinf(goal_.theta_)*goal_.x_-cosf(goal_.theta_)*goal_.y_;
+
+    float rho = sqrt(x_rp*x_rp+y_rp*y_rp);
+    float alpha = -theta+atan2f(-y_rp, -x_rp);
+
+    /*Make sure that alpha is between in [-pi, pi]*/
+    int n = (int) alpha/(2.0*M_PI);
+    if(n != 0)
+        alpha = alpha-n*2.0*M_PI;
+    if(alpha > M_PI)
+        alpha = alpha-2.0*M_PI;
+    else if(alpha < -M_PI)
+        alpha = alpha+2.0*M_PI;
+
+    float v = k_ro_*rho;
+
+
+    /*Check if alpha belongs to I1 or I2 so that we can choose backward or forward parking*/
+    if(!(alpha <= M_PI_2 && alpha > -M_PI_2))
+    {
+        /*If the robot is parking backward*/
+        v = -v;
+       
+        if(alpha > M_PI_2)
+        {
+            alpha = alpha -M_PI;
+        }else if(alpha <= -M_PI_2)
+        {
+            alpha = alpha + M_PI;
+        }
+        
+    }
+
+    float beta = -theta-alpha;
+    /*Make sure that beta is between in [-pi, pi]*/
+    n = (int) beta/(2*M_PI);
+
+    if(n != 0)
+        beta =  beta-n*2.0*M_PI;
+    if(beta > M_PI)
+        beta = beta-2.0*M_PI;
+    else if(beta < -M_PI)
+        beta = beta+2.0*M_PI;
+
+    float w =  k_alpha_*alpha+k_beta_*beta;
+
+    /*Noubakhsh and Siegwart do not consider actuation limits (saturating velocities).
+    If one of the desired velocities is higher than what motor could deliver, we need to recompute
+    the gains to ensure the strong stability condition (and get a predictable behavior)*/
+
+    if(fabs(w) > ang_vel_sat_ || fabs(v) > lin_vel_sat_)
+    {
+
+        double w_ratio = fabs(ang_vel_sat_/w);
+        double v_ratio = fabs(lin_vel_sat_/v);
+
+        double scale = std::min(v_ratio, w_ratio);
+        
+        /*since the control signal results from a linear combination of the gains and they are all scaled
+        in the same way, we just have to scale the control signals*/
+
+        v = v*scale;
+        w =  w*scale;
+        
+    }
+
+    return ControlSignal(v, w);
 }
+
+Pose2D DockingController::getCurrentGoal()
+{
+    return goal_;
+}
+
+Pose2D DockingController::getCurrentPose()
+{
+    return robot_pose_;
+}
+
+void DockingController::updateRobotPose(float x, float y, float theta, std::string frame_id)
+{
+    robot_pose_.x_ = x;
+    robot_pose_.y_ = y;
+    robot_pose_.theta_ = theta;
+    robot_pose_.frame_id_ = frame_id;
+}
+
+void DockingController::updateRobotPose(Pose2D newPose)
+{
+    robot_pose_ = newPose;
+}
+
+
+void DockingController::updateGoal(float x, float y, float theta, std::string frame_id)
+{
+    goal_.x_ = x;
+    goal_.y_ = y;
+    goal_.theta_ = theta;
+    goal_.frame_id_ = frame_id;
+}
+
+
+void DockingController::updateGoal(Pose2D newGoal)
+{
+    goal_ = newGoal;
+}
+
+void DockingController::updateSaturations(float lin_vel_sat, float ang_vel_sat)
+{
+    lin_vel_sat_ = lin_vel_sat;
+    ang_vel_sat_ = ang_vel_sat;
+}
+
+
+//END namespace docking_ctrl
+}
+
 
 
