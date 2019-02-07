7aa56347463c9823e3471a0a2e8c643ce23a2f5c
ruifigueiredo <ruihortafigueiredo@gmail.com>
2019-02-07 09:39:59 +0000

    using PPF features plus icp for docking pattern pose estimation using laser info

---
+++ vizzy_navigation/include/pattern_pose_estimation.hpp
@@ -0,0 +1,56 @@
+#ifndef PATTERN_POSE_ESTIMATION_HPP_
+#define PATTERN_POSE_ESTIMATION_HPP_
+
+//#include <pcl/features/ppf.h>
+//#include <pcl/io/pcd_io.h>
+//#include <pcl/filters/voxel_grid.h>
+//#include <pcl/features/normal_3d.h>
+#include <pcl/registration/ppf_registration.h>
+#include <pcl/registration/icp.h>
+#include <pcl/common/io.h>
+#include <pcl/console/print.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/io/ascii_io.h>
+#include <pcl/io/vtk_lib_io.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+using namespace pcl;
+using namespace std;
+
+class PatternPoseEstimation
+{
+	vector<pcl::PPFHashMapSearch::Ptr> hashmap_search_vector;
+
+	double rot_thresh;
+	double tran_thresh;
+	double cluster_tran_thresh;
+	double cluster_rot_thresh;
+	double fitting_score_thresh;
+	pcl::PointCloud<pcl::Normal>::Ptr normals_;
+	pcl::PointCloud<pcl::Normal>::Ptr normals;
+	pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud_;
+    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree;
+	public:
+		PointCloud<PointNormal>::Ptr cloud_output_subsampled;
+
+		pcl::PointCloud<PointNormal>::Ptr getPointNormal(pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud);
+
+		std::vector<pcl::PointCloud<pcl::PointNormal>::Ptr> dense_cloud_models;
+		std::vector<pcl::PointCloud<pcl::PointNormal>::Ptr> cloud_models_with_normals;
+
+		PatternPoseEstimation(	double rot_thresh_=30.0, 
+								double tran_thresh_=0.05,
+								double fitting_score_thresh_=0.01, 
+								std::string file_="file");
+
+		int train (std::vector<pcl::PointCloud<pcl::PointNormal>::Ptr> cloud_models_with_normals_);
+		Eigen::Affine3d detect(pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals);
+
+		Eigen::Matrix4f refine(pcl::PointCloud<pcl::PointNormal>::Ptr cloud_in, pcl::PointCloud<pcl::PointNormal>::Ptr cloud_target);
+		void loadModelFromMesh(std::string file_name="filename");
+};
+
+#endif
