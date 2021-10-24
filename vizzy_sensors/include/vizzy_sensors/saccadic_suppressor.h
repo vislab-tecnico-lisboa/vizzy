933550a1a82d65c011da4d68eb9b13f05eeb3f00
João Avelino <joao.avelino@tecnico.ulisboa.pt>
2021-10-24 11:44:26 +0100

    Implemented saccadic suppression nodelet based on: laplacian variation on images, wheel velocity. Uses dynamic reconfigure.

--- vizzy_sensors/include/vizzy_sensors/saccadic_suppressor.h
+++ vizzy_sensors/include/vizzy_sensors/saccadic_suppressor.h
@@ -7,8 +7,14 @@
 #include <image_transport/image_transport.h>
 #include <mutex>
 #include <cv_bridge/cv_bridge.h>
+#include "opencv2/imgproc.hpp"
+#include <dynamic_reconfigure/server.h>
+#include <vizzy_sensors/SuppressionConfig.h>
+#include <std_msgs/Bool.h>
+#include <geometry_msgs/Twist.h>
 
-/*Nodelet code heavily based on the image_proc cpp nodelet code*/
+
+/*Nodelet code heavily based on the image_proc cpp resize nodelet code*/
 
 
 namespace vizzy_sensors
@@ -22,11 +28,25 @@ protected:
     std::shared_ptr<ros::NodeHandle> nPriv_;
     image_transport::Publisher pub_image_;
     ros::Publisher pub_info_;
+    ros::Publisher pub_status_;
     image_transport::Subscriber sub_image_;
     ros::Subscriber sub_info_;
 
+    //Checks the base velocity command
+    ros::Subscriber sub_cmdvel_;
+    geometry_msgs::Twist last_cmdvel_;
+    std::mutex  angvel_mutex_;
+
     std::shared_ptr<image_transport::ImageTransport> it_, private_it_;
     std::mutex connect_mutex_; 
+
+    // Dynamic reconfigure
+    std::mutex config_mutex_;
+    typedef vizzy_sensors::SuppressionConfig Config;
+    typedef dynamic_reconfigure::Server<Config> ReconfigureServer;
+    std::shared_ptr<ReconfigureServer> reconfigure_server_;
+    Config config_;
+
 
 public:
     SaccadicSuppression(){}
@@ -38,6 +58,11 @@ public:
     void imageCb(const sensor_msgs::ImageConstPtr& image_msg);
     void infoCb(const sensor_msgs::CameraInfoConstPtr& info_msg);    
 
+    void cmdVelCb(const geometry_msgs::Twist& cmdvel_msg);
+
+    double getAngvel();
+
+    void configCb(Config& config, uint32_t level);
 
 };
 
